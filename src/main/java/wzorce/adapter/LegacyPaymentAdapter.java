c99feb2fee3cd9964d323ea83e55430393957db8
Artur Laskowski <artur.laskowski@comarch.com>
2024-11-23 08:57:53 +0100

    adapter-zad

--- src/main/java/wzorce/adapter/LegacyPaymentAdapter.java
+++ src/main/java/wzorce/adapter/LegacyPaymentAdapter.java
@@ -1,9 +1,25 @@
 package wzorce.adapter;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConditionalOnProperty(name = "payment.system", havingValue = "legacy")
-class LegacyPaymentAdapter {
+@RequiredArgsConstructor
+class LegacyPaymentAdapter implements PaymentProcessor {
+
+    private final LegacyPaymentService legacyPaymentService;
+
+    @Override
+    public void processPayment(NewSystemPaymentDto newSystemPaymentDto) {
+        var legacyPaymentDto = new LegacyPaymentDto(newSystemPaymentDto.accountNumber()
+                , newSystemPaymentDto.transactionId(), newSystemPaymentDto.amount().intValue());
+
+        if (Currency.PLN != newSystemPaymentDto.currency()) {
+            throw new UnsupportedCurrencyException(newSystemPaymentDto.currency());
+        }
+
+        legacyPaymentService.makePayment(legacyPaymentDto);
+    }
 }
