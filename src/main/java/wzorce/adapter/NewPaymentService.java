255a3a2f30feb57a08f757756dd80c1e7f307cf7
Artur Laskowski <artur.laskowski@comarch.com>
2024-11-23 08:57:53 +0100

    adapter-zad

--- src/main/java/wzorce/adapter/NewPaymentService.java
+++ src/main/java/wzorce/adapter/NewPaymentService.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @ConditionalOnProperty(name = "payment.system", havingValue = "new", matchIfMissing = true)
-class NewPaymentService {
+class NewPaymentService implements PaymentProcessor {
 
     private final PaymentRepository paymentRepository;
 
+    @Override
     public void processPayment(NewSystemPaymentDto newSystemPaymentDto) {
         var payment = new Payment(newSystemPaymentDto.accountNumber(), newSystemPaymentDto.transactionId(),
                 newSystemPaymentDto.amount(), newSystemPaymentDto.currency(), PaymentSystem.NEW);
